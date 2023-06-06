4246da1716826b9f68f5e3150df2d6e80712e7ee
Fernando Henrique Oliveira Duarte <hodfernando@gmail.com>
2023-06-05 22:55:17 -0300

    Requirements atualizado.

--- BRACIS_2023/GNN_EXPLAINER.py
+++ BRACIS_2023/GNN_EXPLAINER.py
@@ -1,7 +1,7 @@
 import os
-import numpy as np
-from torch_geometric.contrib.explain import PGMExplainer
-from torch_geometric.explain import Explainer, ModelConfig
+import pandas as pd
+from torch_geometric.explain import Explainer, GNNExplainer
+from torch_geometric.utils import k_hop_subgraph
 from torch_geometric_temporal.signal import temporal_signal_split
 from create_dataframe import DatasetLoader
 from models.gnn_lstm_model import LSTMGCN
@@ -9,9 +9,10 @@ from models.gnn_rnn_model import RecurrentGCN
 import torch
 from tqdm import tqdm
 from pathlib import Path
-import matplotlib.pyplot as plt
 import pickle
 import bz2
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import numpy as np
 
 
 def gnnexplainerpredict():
@@ -32,8 +33,8 @@ def gnnexplainerpredict():
     else:
         datasetLoader = DatasetLoader()
         dataset, dataset_standardized = datasetLoader.get_dataset(lags=lags, datasets=datasets, weight=weight, out=out)
-        file = bz2.BZ2File(mydir + '/save/class/' + filename_dataset, 'wb')
-        # pickle.dump(datasetLoader, file)
+        with bz2.BZ2File(mydir + '/save/class/' + filename_dataset, 'wb') as file:
+            pickle.dump(datasetLoader, file)
         file.close()
         print('Dataset carregado e gerado arquivo pickle')
 
@@ -68,34 +69,74 @@ def gnnexplainerpredict():
 
         model.eval()
 
+        # for explanation_type in ['phenomenon', 'model']:
         explainer = Explainer(
-            model=model, algorithm=PGMExplainer(), node_mask_type='attributes',
-            explanation_type='phenomenon',
-            model_config=ModelConfig(mode='regression',
-                                     task_level='node', return_type='raw'))
+            model=model,
+            algorithm=GNNExplainer(epochs=10),
+            explanation_type='model',
+            node_mask_type='attributes',
+            edge_mask_type='object',
+            model_config=dict(
+                mode='regression',
+                task_level='node',
+                return_type='raw',
+            ),
+        )
 
-        time = 0
-        for time, snapshot in enumerate(test_dataset):
-            y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
-            break
+        # SP/SP - Sudeste - 3550308
+        # Manaus/AM - Norte - 1302603
+        # Fortaleza/CE - Nordeste - 2304400
+        # Brasília/DF - Centro - 5300108
+        # Florianópolis/SC - Sul - 4205407
+        node_indices = datasetLoader._label_encoder_CODMUNDV.transform([3550308, 1302603, 5300108])
+        targets, preds, results = [], [], []
+        mae, mse, rmse, r2 = 0, 0, 0, 0
+        for node_index in tqdm(node_indices, leave=False, desc='Test Explainer'):
+            for time, snapshot in enumerate(test_dataset):
+                target = snapshot.y  # if explanation_type == 'phenomenon' else None
+                explanation = explainer(x=snapshot.x, edge_index=snapshot.edge_index, index=node_index,
+                                        target=target, edge_weight=snapshot.edge_attr)
 
-        node_idx = 0
+                subset, _, _, _ = k_hop_subgraph(int(node_index), num_hops=1, edge_index=snapshot.edge_index)
 
-        explanation = explainer(x=snapshot.x, edge_index=snapshot.edge_index, index=node_idx,
-                                target=y_hat, edge_weight=snapshot.edge_weight)
+                targets.append(snapshot.subgraph(subset).y)
+                preds.append(explanation.subgraph(subset).target.squeeze())
 
-        significance = explanation.pgm_stats
-        print(f'Significance of relevant neighbors: {significance}')
+                mae += mean_absolute_error(targets[time], preds[time])
+                mse += mean_squared_error(targets[time], preds[time])
+                rmse += np.sqrt(mse)
+                r2 += r2_score(targets[time], preds[time])
+            mae /= time
+            mse /= time
+            rmse /= time
+            r2 /= time
+            # print(f"Node: {node_index}")
+            # print(f"MAE: {mae}")
+            # print(f"MSE: {mse}")
+            # print(f"RMSE: {rmse}")
+            # print(f"R²: {r2}")
 
-        nodes = np.arange(significance.shape[0])
+            result = {
+                'Node': node_index,
+                'MAE': mae,
+                'MSE': mse,
+                'RMSE': rmse,
+                'R²': r2
+            }
+            results.append(result)
 
-        # Gráfico de dispersão
-        plt.figure(figsize=(10, 6))
-        plt.scatter(nodes, significance)
-        plt.xlabel('Nós')
-        plt.ylabel('Significância')
-        plt.title(f'Gráfico de Dispersão - Significância dos Nós em relação ao Nó {node_idx}')
-        plt.show()
+        df_results = pd.DataFrame(results)
+        print(df_results)
+
+        # nodes = np.arange(significance.shape[0])
+        #
+        # # Gráfico de dispersão
+        # plt.figure(figsize=(10, 6))
+        # plt.scatter(nodes, significance)
+        # plt.xlabel('Nós')
+        # plt.ylabel('Significância')
+        # plt.title(f'Gráfico de Dispersão - Significância dos Nós em relação ao Nó {node_idx}')
+        # plt.show()
 
 
 # Setuando e chamando
