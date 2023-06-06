f1e232abca431d9bd9fab85abecc33716a8e3922
Fernando Henrique Oliveira Duarte <hodfernando@gmail.com>
2023-06-06 11:41:25 -0300

    Gerando resultados para o BRACIS

--- BRACIS_2023/GNN_EXPLAINER.py
+++ BRACIS_2023/GNN_EXPLAINER.py
@@ -14,66 +14,74 @@ import bz2
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import numpy as np
 
+# Setando e chamando
+lags = 14
+datasets = '2020-2022'
+weight = 'all'
+out = 'cases'
+nameModel = 'GCRN'
+reps = 1
+train_ratio = 0.8
 
-def gnnexplainerpredict():
-    # Diretorios
-    mydir = os.getcwd()
-    Path(mydir + '/save/class/').mkdir(parents=True, exist_ok=True)
-    files_class = os.listdir(mydir + '/save/class/')
+# def gnnexplainerpredict():
+# Diretorios
+mydir = os.getcwd()
+Path(mydir + '/save/class/').mkdir(parents=True, exist_ok=True)
+files_class = os.listdir(mydir + '/save/class/')
 
-    # Carregando Dataset
-    filename_dataset = f'DATASET_lags_{lags}_datasets_{datasets}_weight_{weight}_out_{out}_'
-    if filename_dataset in files_class:
-        file = bz2.BZ2File(mydir + '/save/class/' + filename_dataset, 'rb')
-        datasetLoader = pickle.load(file)
-        dataset, dataset_standardized = datasetLoader.get_dataset(lags=lags, datasets=datasets,
-                                                                  weight=weight, out=out)
-        file.close()
-        print('Dataset carregado pelo pickle data')
-    else:
-        datasetLoader = DatasetLoader()
-        dataset, dataset_standardized = datasetLoader.get_dataset(lags=lags, datasets=datasets, weight=weight, out=out)
-        with bz2.BZ2File(mydir + '/save/class/' + filename_dataset, 'wb') as file:
-            pickle.dump(datasetLoader, file)
-        file.close()
-        print('Dataset carregado e gerado arquivo pickle')
+# Carregando Dataset
+filename_dataset = f'DATASET_lags_{lags}_datasets_{datasets}_weight_{weight}_out_{out}_'
+if filename_dataset in files_class:
+    file = bz2.BZ2File(mydir + '/save/class/' + filename_dataset, 'rb')
+    datasetLoader = pickle.load(file)
+    dataset, dataset_standardized = datasetLoader.get_dataset(lags=lags, datasets=datasets,
+                                                              weight=weight, out=out)
+    file.close()
+    print('Dataset carregado pelo pickle data')
+else:
+    datasetLoader = DatasetLoader()
+    dataset, dataset_standardized = datasetLoader.get_dataset(lags=lags, datasets=datasets, weight=weight, out=out)
+    with bz2.BZ2File(mydir + '/save/class/' + filename_dataset, 'wb') as file:
+        pickle.dump(datasetLoader, file)
+    file.close()
+    print('Dataset carregado e gerado arquivo pickle')
 
-    print("Separando dataset em teste e treinamento")
+print("Separando dataset em teste e treinamento")
 
-    train_dataset, test_dataset = temporal_signal_split(dataset_standardized, train_ratio=train_ratio)
+train_dataset, test_dataset = temporal_signal_split(dataset_standardized, train_ratio=train_ratio)
 
-    for rep in range(reps):
-        # Carregando Model
-        if nameModel == 'GCRN':
-            model = RecurrentGCN(node_features=lags, filters=128)
-        if nameModel == 'GCLSTM':
-            model = LSTMGCN(node_features=lags, filters=128)
+for rep in range(reps):
+    # Carregando Model
+    if nameModel == 'GCRN':
+        model = RecurrentGCN(node_features=lags, filters=128)
+    if nameModel == 'GCLSTM':
+        model = LSTMGCN(node_features=lags, filters=128)
 
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.01)  # adam aleatorio?
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)  # adam aleatorio?
 
-        model.train()
-        print("Treinamento")
-        time = 0
-        for epoch in tqdm(range(100)):
-            cost = 0
-            for time, snapshot in enumerate(train_dataset):  # Shuffle
-                y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
-                cost = torch.mean((y_hat - snapshot.y) ** 2)
-                cost.backward()
-                optimizer.step()
-                optimizer.zero_grad()
+    model.train()
+    print("Treinamento")
+    time = 0
+    for epoch in tqdm(range(100)):
+        cost = 0
+        for time, snapshot in enumerate(train_dataset):  # Shuffle
+            y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
+            cost = torch.mean((y_hat - snapshot.y) ** 2)
+            cost.backward()
+            optimizer.step()
+            optimizer.zero_grad()
 
-        print("Fim do treinamento")
+    print("Fim do treinamento")
 
-        print("Teste")
+    print("Teste")
 
-        model.eval()
-
-        # for explanation_type in ['phenomenon', 'model']:
+    model.eval()
+    results = []
+    for explanation_type in ['phenomenon', 'model']:
         explainer = Explainer(
             model=model,
-            algorithm=GNNExplainer(epochs=10),
-            explanation_type='model',
+            algorithm=GNNExplainer(epochs=100),
+            explanation_type=explanation_type,
             node_mask_type='attributes',
             edge_mask_type='object',
             model_config=dict(
@@ -89,34 +97,36 @@ def gnnexplainerpredict():
         # Brasília/DF - Centro - 5300108
         # Florianópolis/SC - Sul - 4205407
         node_indices = datasetLoader._label_encoder_CODMUNDV.transform([3550308, 1302603, 5300108])
-        targets, preds, results = [], [], []
-        mae, mse, rmse, r2 = 0, 0, 0, 0
         for node_index in tqdm(node_indices, leave=False, desc='Test Explainer'):
+            targets, preds = [], []
+            mae, mse, rmse, r2 = 0, 0, 0, 0
             for time, snapshot in enumerate(test_dataset):
-                target = snapshot.y  # if explanation_type == 'phenomenon' else None
+                target = snapshot.y.unsqueeze(1) if explanation_type == 'phenomenon' else None
                 explanation = explainer(x=snapshot.x, edge_index=snapshot.edge_index, index=node_index,
                                         target=target, edge_weight=snapshot.edge_attr)
 
-                subset, _, _, _ = k_hop_subgraph(int(node_index), num_hops=1, edge_index=snapshot.edge_index)
+                subset, edge_index, mapping, edge_mask = k_hop_subgraph(int(node_index), num_hops=1,
+                                                                        edge_index=snapshot.edge_index)
+                targets.append((snapshot.subgraph(
+                    subset).y * datasetLoader.std_stacked_dataset[subset]) + datasetLoader.mean_stacked_dataset[subset])
+                preds.append((explanation.subgraph(
+                    subset).target.squeeze() * datasetLoader.std_stacked_dataset[subset]) +
+                             datasetLoader.mean_stacked_dataset[subset])
 
-                targets.append(snapshot.subgraph(subset).y)
-                preds.append(explanation.subgraph(subset).target.squeeze())
+                # explanation.visualize_graph('subgraph.png')
+                # explanation.visualize_feature_importance('feature_importance.png', top_k=10)
 
                 mae += mean_absolute_error(targets[time], preds[time])
                 mse += mean_squared_error(targets[time], preds[time])
                 rmse += np.sqrt(mse)
                 r2 += r2_score(targets[time], preds[time])
-            mae /= time
-            mse /= time
-            rmse /= time
-            r2 /= time
-            # print(f"Node: {node_index}")
-            # print(f"MAE: {mae}")
-            # print(f"MSE: {mse}")
-            # print(f"RMSE: {rmse}")
-            # print(f"R²: {r2}")
+            mae /= (time + 1)
+            mse /= (time + 1)
+            rmse /= (time + 1)
+            r2 /= (time + 1)
 
             result = {
+                'Explanation': explanation_type,
                 'Node': node_index,
                 'MAE': mae,
                 'MSE': mse,
@@ -125,26 +135,17 @@ def gnnexplainerpredict():
             }
             results.append(result)
 
-        df_results = pd.DataFrame(results)
-        print(df_results)
+    df_results = pd.DataFrame(results)
+    print()
+    print(df_results)
+    df_results.to_csv('explanation.csv')
 
-        # nodes = np.arange(significance.shape[0])
-        #
-        # # Gráfico de dispersão
-        # plt.figure(figsize=(10, 6))
-        # plt.scatter(nodes, significance)
-        # plt.xlabel('Nós')
-        # plt.ylabel('Significância')
-        # plt.title(f'Gráfico de Dispersão - Significância dos Nós em relação ao Nó {node_idx}')
-        # plt.show()
-
-
-# Setuando e chamando
-lags = 14
-datasets = '2020-2022'
-weight = 'all'
-out = 'cases'
-nameModel = 'GCRN'
-reps = 1
-train_ratio = 0.8
-gnnexplainerpredict()
+    # nodes = np.arange(significance.shape[0])
+    #
+    # # Gráfico de dispersão
+    # plt.figure(figsize=(10, 6))
+    # plt.scatter(nodes, significance)
+    # plt.xlabel('Nós')
+    # plt.ylabel('Significância')
+    # plt.title(f'Gráfico de Dispersão - Significância dos Nós em relação ao Nó {node_idx}')
+    # plt.show()
--- basics/gnn_explainer_ba_shapes/gnn_explainer_ba_shapes.py
+++ basics/gnn_explainer_ba_shapes/gnn_explainer_ba_shapes.py
@@ -86,11 +86,11 @@ for explanation_type in ['phenomenon', 'model']:
         explanation = explainer(data.x, data.edge_index, index=node_index,
                                 target=target)
 
-        _, _, _, hard_edge_mask = k_hop_subgraph(node_index, num_hops=3,
+        subset, edge_index, mapping, edge_mask = k_hop_subgraph(node_index, num_hops=3,
                                                  edge_index=data.edge_index)
 
-        targets.append(data.edge_mask[hard_edge_mask].cpu())
-        preds.append(explanation.edge_mask[hard_edge_mask].cpu())
+        targets.append(data.edge_mask[edge_mask].cpu())
+        preds.append(explanation.edge_mask[edge_mask].cpu())
 
     auc = roc_auc_score(torch.cat(targets), torch.cat(preds))
     print(f'Mean ROC AUC (explanation type {explanation_type:10}): {auc:.4f}')
